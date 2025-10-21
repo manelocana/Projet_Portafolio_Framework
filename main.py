6af09bc3a4f3b9b5cfe4ca1018de4aee03661531
manelocana <manelocana.dev@gmail.com>
2025-10-21 20:53:56 +0200

    routes header nav-menu

--- main.py
+++ main.py
@@ -6,7 +6,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('user/home.html')
+    return render_template('/user/home.html')
+
+
+@app.route('/pages')
+def pages():
+    return render_template('/user/pages.html')
+
+
+@app.route('/portafolio')
+def portafolio():
+    return render_template('/user/portafolio.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('/user/blog.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('/user/contact.html')
+
 
 
 
